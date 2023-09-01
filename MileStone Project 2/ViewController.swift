4eb20a0babacce9b71834d72ed8225858b53b0f9
avi7501 <avinashmuralidharan2@gmail.com>
2023-09-01 12:00:10 +0530

    Initial Commit

---
+++ MileStone Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MileStone Project 2
+//
+//  Created by Avinash Muralidharan on 01/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
